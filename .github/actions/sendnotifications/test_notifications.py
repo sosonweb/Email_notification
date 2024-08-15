0feecadaa1007eb1e45e7831f0b1593fbbec4007
Akhilmalakareddy <148373425+Akhilmalakareddy@users.noreply.github.com>
2024-08-15 15:26:30 -0700

    Update test_notifications.py

--- .github/actions/sendnotifications/test_notifications.py
+++ .github/actions/sendnotifications/test_notifications.py
@@ -13,7 +13,7 @@ from main import send_email_notification  # Replace with your actual module name
     'NOTIFY_FLAGS': '{"send-teams-notification": true}'
 })
 def test_send_email_notification_with_all_vars(mock_smtp):
-    print(os.environ)
+    #print(os.environ)
     # Define test data
     message = "<p>This is a test message</p>"
     recipients = ["test@example.com"]
@@ -41,7 +41,7 @@ def test_send_email_notification_with_all_vars(mock_smtp):
     'NOTIFICATION_MAP': '{}',
     'APP_TYPE': 'web',
     'BUILD_URL': 'http://build-url.com',
-    'NOTIFY_FLAGS': '{"send-teams-notification": true}',
+    'NOTIFY_FLAGS': '{"send-teams-notification": false}',
     'LOG_LEVEL': '20'
 })
 
@@ -62,35 +62,6 @@ def test_send_email_notification_no_recipients(mock_smtp):
     mock_smtp.assert_not_called()  # No SMTP actions should be performed
     mock_smtp_instance.quit.assert_not_called()  # No SMTP actions should be performed
 
-@patch.dict(os.environ, {
-    'PROJECT_GIT_REPO': 'test-repo',
-    'NOTIFICATION_MAP': '{"email_recipients": ["test@example.com"], "subject": "Test Subject", "message": "Test Message"}',
-    'APP_TYPE': 'web',
-    'BUILD_URL': 'http://build-url.com',
-    'NOTIFY_FLAGS': '{"send-teams-notification": true}'  # Empty notify flags
-})
-@patch('main.smtplib.SMTP')
-def test_send_email_notification_with_empty_notify_flags(mock_smtp):
-    # Define test data
-    message = "<p>This is a test message</p>"
-    recipients = ["test@example.com"]
-    email_subject = "Test Subject"
-
-    # Mock the SMTP instance to prevent sending real emails
-    mock_smtp_instance = mock_smtp.return_value
-    mock_smtp_instance.sendmail = MagicMock()
-
-    # Call the function under test
-    send_email_notification(message, recipients, email_subject)
-
-    # Assertions
-    mock_smtp.assert_called_once_with('mta.kp.org', 25)
-    mock_smtp_instance.sendmail.assert_called_once_with(
-        from_addr='githubactions@kp.org',
-        to_addrs='test@example.com',
-        msg=mock_smtp_instance.sendmail.call_args[1]['msg']
-    )
-    mock_smtp_instance.quit.assert_called_once()
 
 if __name__ == "__main__":
     pytest.main()
