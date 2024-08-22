9e30531af0ec16f29094e127540fb59638da997d
Akhilmalakareddy <148373425+Akhilmalakareddy@users.noreply.github.com>
2024-08-22 01:34:09 -0500

    Update test_notifications.py

--- .github/actions/sendnotifications/test_notifications.py
+++ .github/actions/sendnotifications/test_notifications.py
@@ -16,7 +16,6 @@ from unittest import mock
     'NOTIFY_FLAGS': '{"send-teams-notification": true}'
 })
 def test_send_email_notification_with_all_vars(mock_smtp):
-    #print(os.environ)
     # Define test data
     message = "<p>This is a test message</p>"
     recipients = ["test@example.com"]
