6408b23ae0d902fbb930e5edd974510e2462c2f7
Akhilmalakareddy <148373425+Akhilmalakareddy@users.noreply.github.com>
2024-08-21 19:22:37 -0500

    Update just_copy.py

--- .github/actions/sendnotifications/just_copy.py
+++ .github/actions/sendnotifications/just_copy.py
@@ -1,63 +1,74 @@
-import pytest
-from main import get_env  # Replace with the actual module name if different
+import subprocess
+import sys
+import os
+import yaml
+import logging
 
-def test_get_env_sandbox(monkeypatch):
-    # Mock the environment variable to simulate a sandbox URL
-    monkeypatch.setenv('GITHUB_SERVER_URL', 'https://sandbox.example.com/repo')
-    
-    # Call the function and check if it returns 'sandbox'
-    assert get_env() == 'sandbox'
+log_level = os.getenv('LOG_LEVEL') if os.getenv('LOG_LEVEL') else '20'
+logging.basicConfig(level=int(log_level), format='%(asctime)s :: %(levelname)s :: %(message)s')
+workspace = os.getenv('GITHUB_WORKSPACE')
+lcov_html_report_path = f"{workspace}/coverage/lcov-report"
 
-def test_get_env_local(monkeypatch):
-    # Mock the environment variable to simulate a localhost URL
-    monkeypatch.setenv('GITHUB_SERVER_URL', 'http://localhost:8000/repo')
-    
-    # Call the function and check if it returns 'local'
-    assert get_env() == 'local'
+def main():
+    operation = sys.argv[1]
+    config_map = yaml.safe_load(sys.argv[2])
+    if operation == 'set-vars':
+        set_vars(config_map)
+    elif operation == 'generate-report':
+        generate_test_reports(config_map)
 
-def test_get_env_staging(monkeypatch):
-    # Mock the environment variable to simulate a staging URL
-    monkeypatch.setenv('GITHUB_SERVER_URL', 'https://staging.example.com/repo')
-    
-    # Call the function and check if it returns 'staging'
-    assert get_env() == 'staging'
+def generate_test_reports(build_var_map):
+    args_test = build_var_map.get('args_test')
+    if args_test:
+        logging.info(f'Generating test report using {args_test}')
+        try:
+            subprocess.run(f'export LOG_LEVEL=ERROR && {args_test}', shell=True, check=True, timeout=3600)
+        except subprocess.CalledProcessError as e:
+            logging.error(f"Test report generation failed: {e}")
+        except subprocess.TimeoutExpired as e:
+            logging.error(f"Test report generation timed out: {e}")
+        except (subprocess.CalledProcessError, subprocess.TimeoutExpired, RuntimeError) as e:
+            logging.error(e)
 
-def test_get_env_production(monkeypatch):
-    # Mock the environment variable to simulate a production URL
-    monkeypatch.setenv('GITHUB_SERVER_URL', 'https://production.example.com/repo')
-    
-    # Call the function and check if it returns 'production' (default case)
-    assert get_env() == 'production'
+    build_group = build_var_map.get('build_group')
+    lcov_report_path = build_group.get('js-lcov-report-path')
+    cobertura = build_group.get('cobertura')
+    cobertura_report_path = None
+    if cobertura is not None:
+        subprocess.run(f'python -m pycobertura show --format html --output coverage/cobertura-coverage.html coverage/cobertura-coverage.xml', shell=True)
+        cobertura_report_path = f"{workspace}/coverage/cobertura-coverage.html"
 
-def test_get_env_empty_url(monkeypatch):
-    # Mock the environment variable to simulate an empty URL (or not set)
-    monkeypatch.setenv('GITHUB_SERVER_URL', '')
-    
-    # Call the function and check if it returns 'production' (default case)
-    assert get_env() == 'production'
+    try:
+        html_report_dir = build_group.get('html-reports').get('pipeline-coverage-report').get('report-dir')
+    except AttributeError as e:
+        html_report_dir = None
+        logging.error(f"No HTML report found: {e}")
 
-def test_get_env_unexpected_url(monkeypatch):
-    # Mock the environment variable to simulate an unexpected URL
-    monkeypatch.setenv('GITHUB_SERVER_URL', 'https://unknown.example.com/repo')
-    
-    # Call the function and check if it returns 'production' (default case)
-    assert get_env() == 'production'
+    if lcov_report_path:
+        logging.info(f"Lcov report path {lcov_report_path}")
+        os.system(f"echo 'lcov-report-path={lcov_report_path}' >> $GITHUB_OUTPUT")
+        logging.info(f"Lcov HTML report path {lcov_html_report_path}")
+        os.system(f"echo 'lcov-html-report-path={lcov_html_report_path}' >> $GITHUB_OUTPUT")
 
-import pytest
-from main import get_env  # Replace with the actual module name if different
+    if cobertura_report_path:
+        logging.info(f"Cobertura report path {cobertura_report_path}")
+        os.system(f"echo 'cobertura-report-path={cobertura_report_path}' >> $GITHUB_OUTPUT")
 
-@pytest.mark.parametrize("git_url, expected_env", [
-    ("https://sandbox.example.com/repo", "sandbox"),
-    ("http://localhost:8000/repo", "local"),
-    ("https://staging.example.com/repo", "staging"),
-    ("https://production.example.com/repo", "production"),
-    ("https://unknown.example.com/repo", "production"),  # Default case
-])
-def test_get_env(monkeypatch, git_url, expected_env):
-    # Use monkeypatch to set the GITHUB_SERVER_URL environment variable
-    monkeypatch.setenv('GITHUB_SERVER_URL', git_url)
-    
-    # Call the function and assert the expected environment is returned
-    assert get_env() == expected_env
+    if html_report_dir:
+        try:
+            html_report_path = subprocess.check_output(f"find {workspace} -depth 1 -type d -name {html_report_dir}", shell=True, text=True).strip()
+            if html_report_path:
+                logging.info(f"HTML report path {html_report_path}")
+                os.system(f"echo 'html-report-path={html_report_path}' >> $GITHUB_OUTPUT")
+        except Exception as e:
+            logging.info(f"Test execution did not produce any HTML reports: {e}")
 
+def set_vars(config_map):
+    runtime_version = config_map.get('runtime_version')
+    if runtime_version:
+        os.system(f"echo 'runtime-version={runtime_version}' >> $GITHUB_OUTPUT")
+    os.system(f"echo 'args-build={config_map['args_build']}' >> $GITHUB_OUTPUT")
+    os.system(f"echo 'args-test={config_map['args_test']}' >> $GITHUB_OUTPUT")
+    os.system(f"echo 'test-flag-enabled={config_map['test_flag_enabled']}' >> $GITHUB_OUTPUT")
 
+main()
